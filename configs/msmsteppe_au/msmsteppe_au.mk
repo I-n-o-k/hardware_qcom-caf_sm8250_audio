8442fb934ff81a39c9b9fbefb3adf9965019fe83
Inok <inok.dr189@gmail.com>
2021-11-13 13:05:45 +0700

    Revert "hal: Add support for audio power policy handling"
    This reverts commit caaf09982d4fb25770aa0bbc4b80a7e5c15a949c.

--- configs/msmsteppe_au/msmsteppe_au.mk
+++ configs/msmsteppe_au/msmsteppe_au.mk
@@ -91,9 +91,6 @@ AUDIO_FEATURE_ENABLED_SILENT_BOOT := true
 endif
 AUDIO_FEATURE_ENABLED_FM_TUNER_EXT := true
 AUDIO_FEATURE_ENABLED_ICC := true
-ifneq ( ,$(filter S 12, $(PLATFORM_VERSION)))
-AUDIO_FEATURE_ENABLED_POWER_POLICY := true
-endif
 ##AUTOMOTIVE_AUDIO_FEATURE_FLAGS
 
 ifneq ($(strip $(TARGET_USES_RRO)), true)
@@ -376,8 +373,7 @@ vendor.audio.feature.wsa.enable=false \
 vendor.audio.feature.audiozoom.enable=false \
 vendor.audio.feature.snd_mon.enable=false \
 vendor.audio.feature.auto_hal.enable=true \
-vendor.audio.feature.synth.enable=true \
-vendor.audio.feature.powerpolicy.enable=true
+vendor.audio.feature.synth.enable=true
 else
 # Non-Generic ODM varient related
 PRODUCT_ODM_PROPERTIES += \
@@ -424,8 +420,7 @@ vendor.audio.feature.wsa.enable=false \
 vendor.audio.feature.audiozoom.enable=false \
 vendor.audio.feature.snd_mon.enable=false \
 vendor.audio.feature.auto_hal.enable=true \
-vendor.audio.feature.synth.enable=true \
-vendor.audio.feature.powerpolicy.enable=true
+vendor.audio.feature.synth.enable=true
 endif
 
 # for HIDL related packages
